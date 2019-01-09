8397ddf0f3c7577635e5619598d27025da5a27f8
Eugene Okhryamkin <eugene.okhryamkin@gmail.com>
2019-01-09 15:04:20 +0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "./index.sass";
+import App from "./containers/App.jsx";
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,42 @@
-const path = require('path');
-
-const PATHS = {
-    source: path.join(__dirname, "src"),
-    build: path.join(__dirname, "build")
-};
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: PATHS.source + '/index.js',
+    entry: "./src/index.jsx",
     output: {
-        path: PATHS.build,
-        filename: "build.js"
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+        publicPath: "/build"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(sass|css)$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader", // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        overlay: true,
+        port: 3000
     }
 };
